5e2f244bcba30a86eb5ff0ea8a780c61e08e341c
jellypuno <jessielaine.punongbayan@broadcom.com>
2020-04-14 11:56:50 +0200

    Cobol Generator

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -4,7 +4,7 @@ import * as mustache from "mustache";
 import * as fs from "fs";
 
 /* Render Compiler.JCL */
-const compiler = fs.readFileSync("./src/cntl/compiler_template.txt").toString();
+const compiler = fs.readFileSync("./src/templates/compiler_template.txt").toString();
 const rendered_compiler = mustache.render(compiler, config);
 
 if (!fs.existsSync("./build")) fs.mkdirSync("./build");
@@ -12,17 +12,22 @@ fs.writeFileSync("./build/compiler.jcl", rendered_compiler);
 console.log("Generated custom JCL to ./build/compiler.jcl");
 
 /* Render Source Program Part 1*/
-const source = fs.readFileSync("./src/source/helowrld.txt").toString();
+const source = fs.readFileSync("./build/source.cbl").toString();
 const rendered_source = mustache.render(source, config);
-
 if (!fs.existsSync("./build")) fs.mkdirSync("./build");
 fs.writeFileSync("./build/source.cbl", rendered_source);
-console.log("Generated COBOL pgm to ./build/source.cbl");
 
-/* Render Run JCL*/
-const runjob = fs.readFileSync("./src/cntl/helotst.txt").toString();
+const source2 = fs.readFileSync("./build/source.cbl").toString();
+const rendered_cobol = mustache.render(source2, config);
+
+if (!fs.existsSync("./build")) fs.mkdirSync("./build");
+fs.writeFileSync("./build/source.cbl", rendered_cobol);
+console.log("Generated COBOL program to ./build/source.cbl");
+
+/* Render Run JCL */
+const runjob = fs.readFileSync("./src/templates/runjob_template.txt").toString();
 const rendered_runjob = mustache.render(runjob, config);
 
 if (!fs.existsSync("./build")) fs.mkdirSync("./build");
 fs.writeFileSync("./build/runjob.jcl", rendered_runjob);
-console.log("Generated run job to ./build/runjob.jcl");
+console.log("Generated custom JCL to ./build/runjob.jcl");
